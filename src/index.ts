6474808dbe7ccd87e5cc553be5653324047647c4
Ben Lu <ayroblu@gmail.com>
2022-05-09 11:44:57 +0100

    More setup

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,15 @@
-import remark from 'remark'
-import fs from 'fs';
+import { remark } from "remark";
+import fs from "fs";
 
 function run() {
-  const file = fs.readFileStream('filename')
-  const ast = remark.get('filepath');
+  const file = fs.readFileSync("filename");
+  const ast = remark.get("filepath");
   for (let i = 0; i < ast.root.length; ++i) {
     const node = ast.root[i];
-    if (node?.type === 'CodeBlock') {
-      const followingNode = ast.root[i+1];
-      const hasFollowingCodeOutput = followingNode?.type === 'CodeBlock' && followingNode.attr === 'output'
+    if (node?.type === "CodeBlock") {
+      const followingNode = ast.root[i + 1];
+      const hasFollowingCodeOutput =
+        followingNode?.type === "CodeBlock" && followingNode.attr === "output";
       if (hasFollowingCodeOutput) {
         // todo
       }
